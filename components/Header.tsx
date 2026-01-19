cb59b13e2513b5317c304fe5e733e2e5b2f2e635
Ogunsola Tobi <ogunsola.tobi@lmu.edu.ng>
2026-01-19 21:51:07 +0100

    feat: Initialize Next.js project with core layout, UI components, assets, and global styling.

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import React from 'react'
+import NavItems from './NavItems'
+
+const Header = () => {
+  return (
+    <header className='sticky top-0 header'>
+        <div className='container header-wrapper flex items-center justify-between'>
+            <Link href="/">
+                <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer"/>
+            </Link>
+            <nav className='hidden sm:block'>
+                <NavItems />
+            </nav>
+            {/* userDropdown */}
+        </div>
+    </header>
+  )
+}
+
+export default Header
