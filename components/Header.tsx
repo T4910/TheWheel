97ab43fa27a74fc3322a2f2dbc8fdc9597aa21ef
Ogunsola Tobi <ogunsola.tobi@lmu.edu.ng>
2026-01-21 09:08:19 +0100

    Made changes the navbar for mobile & web

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
 import NavItems from './NavItems'
+import UserDropdown from './UserDropdown'
 
 const Header = () => {
   return (
@@ -13,7 +14,7 @@ const Header = () => {
             <nav className='hidden sm:block'>
                 <NavItems />
             </nav>
-            {/* userDropdown */}
+            <UserDropdown />
         </div>
     </header>
   )
